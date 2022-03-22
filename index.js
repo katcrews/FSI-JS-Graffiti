672e4426e5c6b1807f462d26da5e56c2cf1409cc
Kathleen Crews <katfreitas808@gmail.com>
2022-03-21 20:21:43 -0700

    added changes

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'center'
+header.style.fontFamily = 'cursive'
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '150px'
+}
+let footer = document.querySelector('.footer')
+footer.style.borderStyle = 'solid'
+footer.style.backgroundColor = 'beige'
+footer.style.borderRadius = '5px'
+footer.style.fontFamily = 'comic Sans'
+footer.style.fontStyle = 'italic'
